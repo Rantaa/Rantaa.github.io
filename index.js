75bcb5556529ed2eef53b5a0f680ead02a75d3cd
Rantaa <gusranlo@student.gu.se>
2022-12-30 21:28:34 +0100

    change font-size for mobile

--- index.js
+++ index.js
@@ -71,3 +71,15 @@ window.addEventListener("optimizedScroll", function() {
   }
 
 })
+
+window.addEventListener('orientationchange', function () {
+    if (window.orientation == -90) {
+        document.getElementById('orient').className = 'orientright';
+    }
+    if (window.orientation == 90) {
+        document.getElementById('orient').className = 'orientleft';
+    }
+    if (window.orientation == 0) {
+        document.getElementById('orient').className = '';
+    }
+}, true);
